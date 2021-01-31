d7e40496136c352a73afe09dba2782bdf6ecb209
Adam <zealan@gmail.com>
2021-01-30 19:07:34 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client.js', function(req, res){
+	res.sendFile(__dirname + '/client.js');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
