f9f89d61bacf7dd2e38e7504a810775b75c0ca43
Adam <zealan@gmail.com>
2021-01-30 19:17:06 -0500

    rename index to player

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/player', (req, res) => {
+  res.sendFile(__dirname + '/player.html');
 });
 
 app.get('/client.js', function(req, res){
